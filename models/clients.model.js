c69c4c06a4f3abc7b3138cc232d7d0cfe034ed72
Jose Urbina <j.urbina.0183@Gmail.com>
2020-06-03 08:44:31 -0400

    numClien --> numClient

--- models/clients.model.js
+++ models/clients.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const clientsSchema = new Schema({
-    numClien: {
+    numClient: {
         type: Number,
         unique: true,
         required: true
