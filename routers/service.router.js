bd8537aba0e9b8ba25915bda56ec9631a9e5cc58
Jose Urbina <j.urbina.0183@Gmail.com>
2020-06-04 09:40:14 -0400

    no isAdmin 1

--- routers/service.router.js
+++ routers/service.router.js
@@ -5,11 +5,11 @@ const isAdmin = require('../middleware/isadmin')
 const Service = require('../controllers/service.controller')
 
 // acá va el id del cliente que está pidiendo el servico
-router.route('/register/:id').post(isAdmin, Service.register)
+router.route('/register/:id').post( Service.register)
 router.route('/').get(Service.all)
 // router.route('/:id').get(isAdmin, User.viewOne)
 router.route('/update/:id').put( Service.edit)
-router.route('/:id').delete(isAdmin, Service.destroy)
+router.route('/:id').delete( Service.destroy)
 router.route('/:idService/:idUser').put( Service.addWorker)
 router.route('/:idService/:idUser').delete( Service.delWorker)
 
