384959624bcbeb242e2516cd8588bbb7c1d120f5
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-07-17 19:14:02 -0600

    pruebas registro numdeliverynote

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -54,12 +54,12 @@ module.exports = {
 
                     const client = await  Client.findById(id)
                 
-                    const services = await Service.findOne().sort({created_at: -1})
+                    const services = await Service.find()
 
                     console.log('services', services);
 
                     res.send({ services });
-                    
+
                     // let newNumDeliveryNote;
                     // if(services.length > 0){
                     //     newNumDeliveryNote = services.numDeliveryNote + 1;
