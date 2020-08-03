74f7281e463dff9b220bef9373f5ddf49eff71cb
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-08-02 20:51:18 -0500

    deploy

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const fs = require('fs');
 const moment = require('moment')
 // const mongoose = require('mongoose');
 
-app.use(cors());
+//app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
     next();
-});
+  });
 
 const router = express.Router();
 
