d9afd760eb655f0731e9ee0ab795b761ccf7da40
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-08-02 21:01:16 -0500

    deploy

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const cors = require('cors');
 
 
 
@@ -13,7 +14,7 @@ router.route('/').get(User.all)
 router.route('/:id').get( User.viewOne)
 router.route('/update/:id').put( User.edit)
 router.route('/:id').delete( User.destroy)
-router.route('/register').post(User.register)
+router.route('/register').post(cors(), User.register)
 router.route('/confirmation/:token').get(User.confirmation)
 router.route('/login').post(User.login)
 router.route('/emailpassreset').post(User.sendEmailPassReset)
